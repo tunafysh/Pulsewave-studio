33838b1ea77b6bca796bdd669479442f67092f19
tunafysh <hanansmani@gmail.com>
2025-03-14 00:03:32 +0100

    THE API FILLY WORKS YAYAYAYA

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,45 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+    
+Route::middleware('api')->group(function () {
+    Route::get('/users', function () {
+        $users = User::all();
+        if (count($users) == 0) {
+        return response()->json([
+            'status'=> 'error',
+            'message' => 'No users found',
+        ]);
+    } else {
+        return response()->json([
+            'status'=> 'success',
+            'users' => $users,
+        ]);
+    };
+});
+
+Route::post('/users', function () {
+    User::create([
+        'name' => request('name'),
+        'password' => bcrypt(request('password')),
+    ]);
+});
+
+Route::put('/users', function () {
+    $user = User::find(request('id'));
+    if (request('field') == 'password') {
+        $user->password = bcrypt(request('password'));
+    }
+    else{
+        $user->name = request('name');
+    }
+    $user->save();
+});
+
+Route::delete('/users', function () {
+    User::find(request('id'))->delete(); 
+});
+});
+?>
--- routes/web.php
+++ routes/web.php
@@ -1,44 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\User;
-
 
     Route::get('/', function () {
         return redirect('http://localhost:3000');
     });
-
-Route::get('/api/users', function () {
-    $users = User::all();
-    if (count($users) == 0) {
-        return response()->json([
-            'status'=> 'error',
-            'message' => 'No users found',
-        ]);
-    } ;
-});
-
-Route::post('/api/users', function () {
-    User::create([
-        'name' => request('name'),
-        'password' => bcrypt(request('password')),
-    ]);
-});
-
-Route::put('/api/users/{id}&{field}', function ($id, $field) {
-    $user = User::find($id);
-    if ($field == 'password') {
-        $user->password = bcrypt(request('password'));
-    }
-    else{
-        $user->name = request('name');
-    }
-    $user->save();
-});
-
-Route::delete('/api/users/{id}', function ($id) {
-    User::find($id)->delete(); 
-});
-
 ?>
 
